11238b48f64d0d63764e617a23b0fd5e520baece
avdhut <avduthnibandhe@gmail.com>
2020-05-29 03:05:54 +0000

    done

--- test1.py
+++ test1.py
@@ -1,13 +1,13 @@
-    # define functions  
+      
     def add(x, y):  
        """This function adds two numbers"" 
-       return x + y 
+       return x + y
     def subtract(x, y): 
        """This function subtracts two numbers""" 
-       return x - y 
+       return x - y
     def multiply(x, y): 
        """This function multiplies two numbers""" 
-       return x * y 
+       return x * y
     def divide(x, y): 
        """This function divides two numbers"""  
        return x / y  
